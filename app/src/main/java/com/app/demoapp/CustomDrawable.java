089af0a6d4c2562cc4893736bc7808d17ca3b039
Monir Hossain <monir.khan.city@gmail.com>
2024-10-30 20:48:29 +0600

    Work on custom drawable

---
+++ app/src/main/java/com/app/demoapp/CustomDrawable.java
@@ -0,0 +1,44 @@
+package com.app.demoapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CustomDrawable extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_custom_drawable);
+
+        //
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //
+
+
+
+
+    }
+
+
+
+
+
+
+}
